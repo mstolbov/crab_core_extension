08a6254a152d0662adad56d86df6a6dbd9413240
Mikhail Stolbov <mstolbov@gmail.com>
2017-04-03 17:52:56 +0300

    send request to scorer

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+function sendScoreRequest(word) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://crab-core.herokuapp.com/score/"+word, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      var resp = JSON.parse(xhr.responseText);
+      if (resp.valid)
+        new Notification("This word is valid", {body: "\""+resp.word+"\""+" score: "+resp.score})
+      else
+        new Notification("Sorry, this word is not valid :(")
+      end
+    }
+  }
+  xhr.send();
+};
+
 function onClickHandler(info, tab) {
-  new Notification("This word is valid", {body: "Score: 42"})
-  new Notification("Sorry, this word is not valid :(")
+  sendScoreRequest(info.selectionText);
 };
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
