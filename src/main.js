632ad8e064ae38f7b4a313416774d66e8cf38c7a
Mikhail Stolbov <mstolbov@gmail.com>
2017-04-06 21:37:24 +0300

    inject script in active tab

--- src/main.js
+++ src/main.js
@@ -1,15 +1,40 @@
 import Scorer from './scorer';
 
 let scorer = new Scorer({host: 'https://crab-core.herokuapp.com'});
+let storage = chrome.storage.local;
+
+let injectTab = (tab) => {
+  return new Promise((resolve, reject) => {
+    storage.get(`${tab.id}`, (item) => {
+      if (item[tab.id]) {
+        resolve();
+      } else {
+        storage.set({[tab.id]: true});
+        if (tab.url.match(/^(http|https):/i)) {
+          chrome.tabs.executeScript(tab.id, {file: "content-min.js"}, () => {
+            resolve();
+          });
+        }
+      }
+    });
+  });
+};
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-  scorer.getScore(info.selectionText)
+  injectTab(tab)
+  .then(() => scorer.getScore(info.selectionText))
   .then(score => chrome.tabs.sendMessage(tab.id, {score: score}));
 });
 
 chrome.runtime.onInstalled.addListener(() => {
-  let context = 'selection';
-  let title = 'Score word';
-  let id = 'score_word';
-  chrome.contextMenus.create({'title': title, 'contexts':[context], 'id': id});
+  let options = {
+    contexts: ['selection'],
+    title: 'Score word',
+    id: 'score_word'
+  };
+  chrome.contextMenus.create(options);
+});
+
+chrome.tabs.onRemoved.addListener((tab, info) => {
+  storage.remove(`${tab.id}`);
 });
